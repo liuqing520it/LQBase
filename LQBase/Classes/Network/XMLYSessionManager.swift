fb89b6852905e90ddfdabe69a1a91538262b7db9
liuqing <330663384@qq.com>
2018-01-01 23:17:31 +0800

    podspec add subspec and import Alamofire

--- LQBase/Classes/Network/XMLYSessionManager.swift
+++ LQBase/Classes/Network/XMLYSessionManager.swift
@@ -14,28 +14,11 @@ enum RequestType : Int {
 }
 
 class XMLYSessionManager: NSObject {
-
-    lazy var sessionManager : SessionManager = {
-        let manager = SessionManager.default
-        
-        return manager;
-    }()
-    /// 设置http header
-    ///
-    /// - Parameters:
-    ///   - valus:
-    ///   - forHttp:
-    func setValue(valus : String, forHttp : String){
-        
-    }
     
     func request(requestType : RequestType, urlStr:String,parameter:[String:Any],resultBlock:()->(responseObject : Any ,error : Error)){
         
         if requestType == RequestType.Get{
         }
-        
-      
-        
         
     }
     
